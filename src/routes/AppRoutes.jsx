215209e081a632521a44d8433e54b36554852717
Cesar220301 <132873251+Cesar220301@users.noreply.github.com>
2026-03-01 21:47:05 -0600

    Animacion entre rutas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,25 @@
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import ExperienciaView from "../features/experiencia/views/ExperienciaView";
 import SobreMiView from "../features/sobre-mi/views/SobreMiView";
 
-export default function AppRoutes() {
+function AnimatedRouteSwitch() {
+  const location = useLocation();
+
   return (
-    <HashRouter>
-      <Routes>
+    <div key={location.pathname} className="route-transition">
+      <Routes location={location}>
         <Route path="/" element={<SobreMiView />} />
         <Route path="/experiencia" element={<ExperienciaView />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+    </div>
+  );
+}
+
+export default function AppRoutes() {
+  return (
+    <HashRouter>
+      <AnimatedRouteSwitch />
     </HashRouter>
   );
 }
